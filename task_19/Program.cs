7723e5ecf967ea25742601b73a90e9cf39cf5c99
anwind-git <work-git@yandex.ru>
2022-09-23 20:29:28 +0300

    принимает на вход пятизначное число и проверяет, является ли оно палиндромом

---
+++ task_19/Program.cs
@@ -0,0 +1,14 @@
+﻿//  принимает на вход пятизначное число и проверяет, является ли оно палиндромом
+
+Console.Write("Введите число: ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+
+string number2 = number1.ToString();
+char[] revers = number2.ToCharArray();
+Array.Reverse(revers);
+int num = int.Parse(revers);
+
+if (num == number1)
+    Console.WriteLine($"Число: {number1} полиндром");
+else
+    Console.WriteLine($"Число: {number1} не является полиндромом");
