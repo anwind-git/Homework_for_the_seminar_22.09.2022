acb4d3440b71468afa0b49460e030d659fffc972
anwind-git <work-git@yandex.ru>
2022-09-23 20:47:57 +0300

    принимает на вход пятизначное число и проверяет, является ли оно палиндромом

--- task_19/Program.cs
+++ task_19/Program.cs
@@ -6,9 +6,14 @@ int number1 = Convert.ToInt32(Console.ReadLine());
 string number2 = number1.ToString();
 char[] revers = number2.ToCharArray();
 Array.Reverse(revers);
-int num = int.Parse(revers);
 
-if (num == number1)
-    Console.WriteLine($"Число: {number1} палиндром");
+int num = int.Parse(revers);
+int kol = number2.Length;
+
+if (kol > 4 && kol < 6)
+    if (num == number1)
+        Console.WriteLine($"Число: {number1} палиндром");
+    else
+        Console.WriteLine($"Число: {number1} не является палиндромом");
 else
-    Console.WriteLine($"Число: {number1} не является палиндромом");
+    Console.WriteLine("Введено неверное число");
