e641c968c905ee4dd0839575f49c42c9c4418ddf
anwind-git <work-git@yandex.ru>
2022-09-24 12:02:07 +0300

    принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве

---
+++ task_23/Program.cs
@@ -0,0 +1,11 @@
+﻿// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N
+
+Console.Write("Введите число: ");
+int N = Convert.ToInt32(Console.ReadLine());
+int i = 1;
+
+while (i <= N)
+{
+    Console.WriteLine($"Куб числа {i}: {i} * {i} * {i} = {i * i * i}");
+    i++;
+}
